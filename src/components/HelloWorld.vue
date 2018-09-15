7674b62f44408bb8344b6954c53de033423f1bab
Stefan Djokic <stefandjokic@wegrowth.com>
2018-09-15 21:57:35 +0200

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,61 @@
+<template>
+  <Page class="page">
+    <ActionBar class="action-bar" title="Weather App">
+    </ActionBar>
+
+    <StackLayout class="hello-world">
+      <TextField v-model="textFieldValue" @textChange="fetchWeatherInformation" />
+      <TextView editable="false" class="body" textWrap=true v-model="weatherInformation" />
+    </StackLayout>
+
+  </Page>
+</template>
+
+<script>
+  import { API_KEY } from '../utils/APIKey';
+
+  export default {
+    data () {
+      return {
+        textFieldValue: '',
+        cityName: '',
+        countryCode: '',
+        temperature: 0,
+        weatherInformation: ''
+      };
+    },
+
+    watch: {
+      temperature: function() {
+        this.weatherInformation = `It's currently ${this.temperature} degrees Celsius in ${this.cityName}, ${this.countryCode}.`;
+      }
+    },
+
+    methods: {
+      fetchWeatherInformation() {
+        if (this.textFieldValue.length > 3) {
+          fetch(
+            `http://api.openweathermap.org/data/2.5/weather?q=${this.textFieldValue}&APPID=${API_KEY}&units=metric`
+          )
+            .then(res => res.json())
+            .then(json => {
+              if (json.main) {
+                this.temperature = parseInt(json.main.temp);
+                this.cityName = json.name;
+              }
+            });
+        }
+      }
+    }
+  };
+</script>
+
+<style scoped>
+.hello-world {
+  margin: 20;
+}
+
+label {
+  color: red;
+}
+</style>
---
+++ src/utils/APIKey.js
@@ -0,0 +1,1 @@
+export const API_KEY = '1a0fce4108ac964928e2f32a3b2f08a6';

8c63fb78b65f4e64bcafffd167c98b5a78d4e7d1
Stefan Djokic <stefandjokic@wegrowth.com>
2019-01-17 18:14:00 +0100

    Refactored main.js

--- src/main.js
+++ src/main.js
@@ -1,16 +1,10 @@
-import Vue from 'nativescript-vue';
-
-import HelloWorld from './components/HelloWorld';
-
-
-import './styles.scss';
+import Vue from "nativescript-vue";
+import HelloWorld from "./components/HelloWorld";
+import "./styles.scss";
 
 // Uncomment the following to see NativeScript-Vue output logs
 //Vue.config.silent = false;
 
 new Vue({
-
-  render: h => h(HelloWorld),
-
-
+  render: h => h(HelloWorld)
 }).$start();

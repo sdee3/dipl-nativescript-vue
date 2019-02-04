2516229265d580a10a56d53d93e57824c267471b
Stefan Djokic <stefandjokic@wegrowth.com>
2019-02-04 12:34:09 +0100

    Applied minor code formatting

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,53 +1,55 @@
 <template>
   <Page class="page">
-    <ActionBar class="action-bar" title="Weather App">
-    </ActionBar>
+    <ActionBar class="action-bar" title="Weather App"></ActionBar>
 
     <StackLayout class="hello-world">
-      <TextField v-model="textFieldValue" @textChange="fetchWeatherInformation" />
-      <TextView editable="false" class="body" textWrap=true v-model="weatherInformation" />
+      <TextField v-model="textFieldValue" @textChange="fetchWeatherInformation"/>
+      <TextView editable="false" class="body" textWrap="true" v-model="weatherInformation"/>
     </StackLayout>
-
   </Page>
 </template>
 
 <script>
-  import { API_KEY } from '../utils/APIKey';
+import { API_KEY } from "../utils/APIKey";
 
-  export default {
-    data () {
-      return {
-        textFieldValue: '',
-        cityName: '',
-        countryCode: '',
-        temperature: 0,
-        weatherInformation: ''
-      };
-    },
+export default {
+  data() {
+    return {
+      textFieldValue: "",
+      cityName: "",
+      countryCode: "",
+      temperature: 0,
+      weatherInformation: ""
+    };
+  },
 
-    watch: {
-      temperature: function() {
-        this.weatherInformation = `It's currently ${this.temperature} degrees Celsius in ${this.cityName}, ${this.countryCode}.`;
-      }
-    },
+  watch: {
+    temperature: function() {
+      this.weatherInformation = `It's currently ${
+        this.temperature
+      } degrees Celsius in ${this.cityName}, ${this.countryCode}.`;
+    }
+  },
 
-    methods: {
-      fetchWeatherInformation() {
-        if (this.textFieldValue.length > 3) {
-          fetch(
-            `http://api.openweathermap.org/data/2.5/weather?q=${this.textFieldValue}&APPID=${API_KEY}&units=metric`
-          )
-            .then(res => res.json())
-            .then(json => {
-              if (json.main) {
-                this.temperature = parseInt(json.main.temp);
-                this.cityName = json.name;
-              }
-            });
-        }
+  methods: {
+    fetchWeatherInformation() {
+      if (this.textFieldValue.length > 3) {
+        fetch(
+          `http://api.openweathermap.org/data/2.5/weather?q=${
+            this.textFieldValue
+          }&APPID=${API_KEY}&units=metric`
+        )
+          .then(res => res.json())
+          .then(json => {
+            if (json.main) {
+              this.temperature = parseInt(json.main.temp);
+              this.cityName = json.name;
+            }
+          });
       }
     }
-  };
+  }
+};
 </script>
 
 <style scoped>
